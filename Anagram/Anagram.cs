00e7d3e49b8da9b1a05954ab1ece1d91e52160be
Anousone Kaseumsouk and Eddie Harris <anousone@>
2018-06-27 16:43:00 -0700

    add loop to allow user to enter multiple words

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -5,9 +5,12 @@ namespace Anagram
 {
   public class testWord
   {
-  public string test(string testWord)
+  public string testOutput;
+  public string test(string testWord1)
   {
+  string testWord = testWord1.ToLower();
   string sortedWord = "";
+
   List<string>anagramList = new List<string>{};
 
     char[] array = testWord.ToCharArray();
@@ -15,30 +18,37 @@ namespace Anagram
     sortedWord = string.Join("", array);
     anagramList.Add(sortedWord);
 
-    return anagramList[0];
-
-
+    foreach(string wordItem in anagramList)
+    {
+    //  if (testWord == anagramList[wordItem]
+      testOutput += wordItem + " ";
+    }
+    return testOutput;
   }
 }
+
 class AnagramApp
 {
   static void Main()
   {
     testWord word = new testWord();
+  string anagram = "";
+  string testLine = "";
+  string testLine1 = "";
+
+      do {
     Console.WriteLine("Please Enter Anagram");
-    string anagram = Console.ReadLine();
-    //word.test(anagram)
-    Console.WriteLine(word.test(anagram));
-   //Console.WriteLine("Your anagram is " + anagram);
-    if (anagram == "stop")
-    {
-      Console.WriteLine("TEST");
-    }
-    else
-    {
-      Main();
+    anagram = Console.ReadLine();
+
+    testLine = word.test(anagram);
+
+    Console.WriteLine(testLine);
+ } while (anagram != "stop");
+
+
+
+
+    //  Main();
     }
   }
-}
-
 }
