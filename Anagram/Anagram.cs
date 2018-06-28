7b19f1ba0f5f6b86170da06b62bf7391bbe1cc52
Eddie Harris <harrisejm@gmail.com>
2018-06-27 23:41:01 -0700

    add final version

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -5,50 +5,64 @@ namespace Anagram
 {
   public class testWord
   {
-  public string testOutput;
-  public string test(string testWord1)
-  {
-  string testWord = testWord1.ToLower();
-  string sortedWord = "";
+  public string _testOutput;
+  public string _testAnagram;
 
-  List<string>anagramList = new List<string>{};
+  public testWord(string testOutput, string testAnagram)
+  {
+    _testOutput = testOutput;
+    _testAnagram = testAnagram;
+  }
+
+  public string test(string _testOutput, string _testAnagram)
+  {
+
+  string testWord = _testOutput.ToLower();
+  string testA = _testAnagram.ToLower();
+
+  string sortedWord = "";
+  string anagramWord = "";
+
 
     char[] array = testWord.ToCharArray();
     Array.Sort(array);
     sortedWord = string.Join("", array);
-    anagramList.Add(sortedWord);
 
-    foreach(string wordItem in anagramList)
-    {
-    //  if (testWord == anagramList[wordItem]
-      testOutput += wordItem + " ";
+
+    char[] array1 = testA.ToCharArray();
+    Array.Sort(array1);
+    anagramWord = string.Join("", array1);
+
+    string output = "";
+    if (anagramWord == sortedWord)
+      {
+      output += _testOutput+ " IS AN anagram (to end game ENTER: stop)";
     }
-    return testOutput;
-  }
+    else
+    {
+      output += _testOutput+ " IS NOT an Anagram (to end game ENTER: stop)";
+    }
+    return output;
 }
 
 class AnagramApp
 {
   static void Main()
   {
-    testWord word = new testWord();
-  string anagram = "";
-  string testLine = "";
-  string testLine1 = "";
-
+    Console.WriteLine("Please Enter word");
+    string inputW = Console.ReadLine();
+    string inputWord = "";
+    string finalResult = "";
       do {
-    Console.WriteLine("Please Enter Anagram");
-    anagram = Console.ReadLine();
-
-    testLine = word.test(anagram);
-
-    Console.WriteLine(testLine);
- } while (anagram != "stop");
-
+        Console.WriteLine("Please Enter Anagram of first word (to end game ENTER: stop)");
+        inputWord = Console.ReadLine();
+      testWord word = new testWord(inputWord, inputW);
+      finalResult = word.test(inputWord, inputW);
+      Console.WriteLine(finalResult);
+ } while (inputWord != "stop");
 
 
-
-    //  Main();
     }
   }
+}
 }
